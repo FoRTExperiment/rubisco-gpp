219ba510605c4879214d156eee413e4407f763d3
Stephanie Pennington <spenn1@terpmail.umd.edu>
2020-11-30 14:49:42 -0500

    Add folder path argument

--- read_files.R
+++ read_files.R
@@ -4,13 +4,16 @@
 library(readr)
 library(lubridate)
 
-# Function should read in a file path (character) for a Fluxnet site and return a dataframe with timestamp and GPP
+# Function should read in a folder path (character) for a Fluxnet site and return a dataframe with timestamp and GPP
 
-read_gpp <- function(file) {
-
-  x <- read_csv(file)
+read_gpp <- function(folder) {
+  
+  # Locate Fluxnet file in given folder path
+  file_path <- list.files(path = folder, pattern = "FLUXNET")
+  
+  x <- read_csv(folder)
   x %>% 
-    select(TIMESTAMP, GPP_NT_VUT_50) %>% 
+    select(TIMESTAMP, GPP_NT_VUT_50) %>% #started with only timestamp and gpp, can add more variables later
     rename(Timestamp = TIMESTAMP, GPP = GPP_NT_VUT_50) %>% #im not sure which gpp to use right now, so this is a placeholder
     mutate(Timestamp = as_date(as.character(Timestamp), format = "%Y%m%d"))
   
